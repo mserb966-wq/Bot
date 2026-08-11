022854ef2b19ee9e944a36d87e988685641eb968
mserb966-wq <mserb966@gmail.com>
2026-08-11 11:28:34 +0300

    Refactor admin notification message formatting

--- main.py
+++ main.py
@@ -120,20 +120,26 @@ def process_injuries(message):
         f"<b>Травмы/Ограничения:</b> {data['injuries']}\n\n"
         "Creator изучит данные и свяжется с тобой."
     )
-    # Уведомление тебе в личку со всей анкетой клиентов.   
-        if message.from_user.username:
-    user_tag = f"@{message.from_user.username}"
-        else:
-    user_tag = f"<a href='tg://user?id={message.from_user.id}'>{message.from_user.first_name}</a>"
+        # Уведомление тебе в личку со всей анкетой клиента
+    if message.from_user.username:
+        user_tag = f"@{message.from_user.username}"
+    else:
+        user_tag = f"<a href='tg://user?id={message.from_user.id}'>{message.from_user.first_name}</a>"
 
-        admin_text =     (
-    "🚨 <b>НОВАЯ АНКЕТА ПОДОПЕЧНОГО!</b>\n\n"
-    f"<b>Клиент:</b> {user_tag}\n"
-    f"<b>Имя:</b> {data['name']}\n"
-    f"<b>Параметры:</b> {data['age_weight']}\n"
-    f"<b>Цель:</b> {data['goal']}\n"
-    f"<b>Травмы/Ограничения:</b> {data['injury']}"
-        )
+    admin_text = (
+        "🚨 <b>НОВАЯ АНКЕТА ПОДОПЕЧНОГО!</b>\n\n"
+        f"<b>Клиент:</b> {user_tag}\n"
+        f"<b>Имя:</b> {data['name']}\n"
+        f"<b>Параметры:</b> {data['age_weight']}\n"
+        f"<b>Цель:</b> {data['goal']}\n"
+        f"<b>Травмы/Ограничения:</b> {data['injury']}"
+    )
+
+    try:
+        bot.send_message(ADMIN_ID, admin_text, parse_mode="HTML")
+    except Exception as e:
+        print(f"Ошибка отправки админу: {e}")
+        
    
             try:
         bot.send_message(ADMIN_ID, admin_text, parse_mode="HTML")
